1a0d7a5113655fc7f91f07a97cd22f4c54026a38
Aditya Pradita S <pradita397@gmail.com>
2020-08-29 18:20:56 +0700

    Add component search big for homepage only

---
+++ src/components/SearchBig.js
@@ -0,0 +1,35 @@
+//This component is to Render Search Box. Main Purpose is to search movie
+
+import React, {Component} from 'react'; 
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+class SearchBox extends Component {
+
+    render() {
+        var search = function (e) {
+            let searchQuery = document.getElementById('searchBig').value;
+            e.preventDefault();
+            window.location.href=`/search?title=${searchQuery}`;
+        }
+      
+        return (
+            <Form onSubmit={search.bind(this)}>
+                <InputGroup  className="search-input" >
+                    <FormControl
+                    placeholder="Search Movies..."
+                    aria-label="Search Movies..."
+                    aria-describedby="basic-addon2" id="searchBig" required
+                    />
+                    <InputGroup.Append>
+                    <Button type="submit" variant="outline-secondary"><img className="search-icon" src="/icons/search.svg" alt="search-icon"/></Button>
+                    </InputGroup.Append>
+                </InputGroup>
+            </Form>
+
+        )
+    }
+}
+
+export default SearchBox;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,25 @@
 
 import React, {Component} from 'react'; 
 import MovieCard from '../components/MovieCard';
-
+import SearchBig from '../components/SearchBig';
 import MovieSlider from '../components/MovieSlider';
 class Home extends Component {
     render() {
         return (
         <div className ="home-container">
             <h1 className="home-title">CouchPotato - Browse for Your Favourite Movies Here!</h1>
-
+            <div className="container no-pad">
+                <SearchBig />
+            </div>
+            <div className="container">
+                <h2>
+                    Popular Movies
+                </h2>
+            </div>
+            
             <MovieSlider type="popular" count="10"/>
             <div className="container content no-pad">
+
                 <h2>Top Rated Movies</h2>
                 <MovieCard type="top_rated" count="10"/>
                 <h2>Now Playing</h2>
@@ -19,6 +28,7 @@ class Home extends Component {
                 <MovieCard type="now_playing" count="10"/>
 
             </div>
+            
         </div>
         
 
